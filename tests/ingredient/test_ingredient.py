8b370467c5cfa43e42f2a4cabf9fc0948800ad21
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-04-23 20:13:42 -0300

    Resolve requisito 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,5 +3,11 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    # Inicia o projeto!
-    pass
+    carne = Ingredient("Carne")
+    carne2 = Ingredient("Carne")
+    ovo = Ingredient("Ovo")
+    assert carne.name == "Carne"
+    assert repr(carne) == "Ingredient('Carne')"
+    assert carne == carne != ovo
+    assert hash(carne) == hash(carne2) != hash(ovo)
+    assert len(carne.restrictions) == 0
