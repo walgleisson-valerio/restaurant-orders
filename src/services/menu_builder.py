8a9f0ca0c75ad7dea72e82661f90af66fa5ce938
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-04-24 01:00:17 -0300

    Resolve requisito 04

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,16 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+        menu = {
+            "dish_name": [],
+            "ingredients": [],
+            "price": [],
+            "restrictions": [],
+        }
+        for dish in self.menu_data.dishes:
+            if restriction not in dish.get_restrictions():
+                menu["dish_name"].append(dish.name)
+                menu["ingredients"].append(dish.get_ingredients())
+                menu["price"].append(dish.price)
+                menu["restrictions"].append(dish.get_restrictions())
+        return pd.DataFrame(menu)
