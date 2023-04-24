771a7af6e160ee954666cb6286397d6bcae091a6
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-04-24 00:14:56 -0300

    Resolve requisito 03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,18 @@
+import pandas as pd
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = self.set_menu(source_path)
+
+    def set_menu(self, path: str) -> set:
+        df = pd.read_csv(path)
+        dishes = {}
+        for dish, price, ingr, amount in df.itertuples(index=False):
+            if dish not in dishes:
+                dishes[dish] = Dish(dish, price)
+            dishes[dish].add_ingredient_dependency(Ingredient(ingr), amount)
+        return set(dishes.values())
